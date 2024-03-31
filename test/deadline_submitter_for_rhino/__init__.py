f54d75101d42936e42fdf936316f9a85a81d4f44
Morgan Epp <60796713+epmog@users.noreply.github.com>
2024-03-31 15:40:36 -0500

    chore: update deadline dep
    Signed-off-by: Morgan Epp <60796713+epmog@users.noreply.github.com>

--- test/deadline_submitter_for_rhino/__init__.py
+++ test/deadline_submitter_for_rhino/__init__.py
@@ -4,11 +4,15 @@ from unittest.mock import MagicMock
 
 # we must mock UI code
 mock_modules = [
-    "deadline.client.ui.deadline_credentials_status",
+    "deadline.client.ui.deadline_authentication_status",
     "PySide2",
     "PySide2.QtCore",
     "PySide2.QtGui",
     "PySide2.QtWidgets",
+    "qtpy",
+    "qtpy.QtCore",
+    "qtpy.QtWidgets",
+    "qtpy.QtGui",
 ]
 
 for module in mock_modules:
